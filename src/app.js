f805c3d2dc5b19bbf878c1e2ba2745157e2a28e0
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-08-31 01:53:06 +0530

    app.js updated

--- src/app.js
+++ src/app.js
@@ -14,14 +14,14 @@ require('../db/connection');
 
 const Port = process.env.PORT || 8000;
 
-app.use(require('../router/auth'));
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('../client/build'));
-    /* app.get("/", (req, res) => {
+    app.use(express.static('../client/build'))/* ;
+    app.get("/", (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
     }) */
 }
+
+app.use(require('../router/auth'));
 
 app.listen(Port, () => {
     console.log(`Server listening on: http://localhost:${Port}`);
