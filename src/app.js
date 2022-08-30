a2b774fa91702a2298733334a0130739e1fcfdf4
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-08-31 00:54:09 +0530

    route path updated

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const Port = process.env.PORT || 8000;
 app.use(require('../router/auth'));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join('../client/build')));
+    app.use(express.static('../client/build'));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
     })
