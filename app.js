c766756253e92d11e97f27a802187f38a948a7df
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-08-31 17:22:12 +0530

    addition in servers app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const Port = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
